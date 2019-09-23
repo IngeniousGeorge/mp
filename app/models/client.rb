24f67560bd7e02797aea430cf3d53cabe3c71369
ingeniousgeorge <ingeniousgeorge@gmail.com>
2019-09-23 13:53:52 +0200

    clients

---
+++ app/models/client.rb
@@ -0,0 +1,13 @@
+class Client < ApplicationRecord
+  has_one :basket
+  delegate :products, to: :basket, prefix: true
+
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable # , :confirmable
+
+  extend FriendlyId
+  friendly_id :slug_candidates, use: :slugged
+
+  def slug_candidates
+    %i[name email]
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :clients
   # scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
     root "home#index"
     # devise_for :admin_users, ActiveAdmin::Devise.config
