4ab03e170c0c6eac62274683f2c24fe46c2f833d
ingeniousgeorge <ingeniousgeorge@gmail.com>
2019-09-24 16:05:52 +0200

    retry

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,18 +12,15 @@ if Rails.env.development?
   c1 = Client.create!(name: "jim", email: "jim@ig.com", password: "password", password_confirmation: "password", confirmed_at: DateTime.now)
   Basket.create(client_id: c1.id)
   l1 = Location.create(name: "jim norton", street: "Sonnenalle 1", city: "Berlin", country: "Germany", postal_code: "12047", owner_type: "Client", owner_id: c1.id)
-  # ClientLocation.create(location_id: l1.id, client_id: c1.id)
-
 
   c2 = Client.create!(name: "jc", email: "jc@ig.com", password: "password", password_confirmation: "password", confirmed_at: DateTime.now)
   Basket.create(client_id: c2.id)
-  l2 = Location.create(name: "jc", street: "Sonnenalle 200", city: "Berlin", country: "Germany", postal_code: "12059")
-  # ClientLocation.create(location_id: l2.id, client_id: c2.id)
+  l2 = Location.create(name: "jc", street: "Sonnenalle 200", city: "Berlin", country: "Germany", postal_code: "12059", owner_type: "Client", owner_id: c2.id)
 
   s1 = Seller.create(name: "Jane Choco", slug: "jane-choco", email: "jane@sel.com", password: "password", password_confirmation: "password", confirmed_at: DateTime.now)
-  l3 = Location.create(name: "Jane Choco", street: "Weserstraße 100", city: "Berlin", country: "Germany", postal_code: "12047")
-  # SellerLocation.create(location_id: l3.id, seller_id: s1.id)
-  # s1.logo.attach(io: File.open(Rails.root.join("storage")), filename: 'jane-choco_logo.jpeg')
+  l3 = Location.create(name: "Jane Choco", street: "Weserstraße 100", city: "Berlin", country: "Germany", postal_code: "12047", owner_type: "Seller", owner_id: s1.id)
+  # path = Rails.root.join("storage")
+  s1.logo.attach()
   # s1.bg.attach(io: File.open(Rails.root.join("storage")), filename: 'jane-choco_bg.jpeg')
   p1 = Product.create(name: "Chocolate", category: "food", key_words: ["yummy", "great"], description: "Amazing chocolate!!!", price: 100000, price_excl_vat: 80000, seller_id: s1.id)
   # p1.logo.attach(io: File.open(Rails.root.join("storage")), filename: 'jane-choco_chocolate_logo.jpeg')
@@ -33,7 +30,7 @@ if Rails.env.development?
   # p2.logo.attach(io: File.open(Rails.root.join("storage")), filename: 'jane-choco_marmite_logo.jpeg')
 
   s2 = Seller.create(name: "Jack Jack", slug: "jack-jack", email: "jack@sel.com", password: "password", password_confirmation: "password", confirmed_at: DateTime.now)
-  l4 = Location.create(name: "jack jack", street: "Innstraße 10", city: "Berlin", country: "Germany", postal_code: "12045")
+  l4 = Location.create(name: "jack jack", street: "Innstraße 10", city: "Berlin", country: "Germany", postal_code: "12045", owner_type: "Seller", owner_id: s2.id)
   # SellerLocation.create(location_id: l4.id, seller_id: s2.id)
   # s2.logo.attach(io: File.open(Rails.root.join("storage")), filename: 'jack_logo.png')
   # s2.bg.attach(io: File.open(Rails.root.join("storage")), filename: 'jack_bg.jpeg')
