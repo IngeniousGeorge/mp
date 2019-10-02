3b0ed0cc7ff5c84ec34cf6d070f8a16e98335a37
ingeniousgeorge <ingeniousgeorge@gmail.com>
2019-10-02 12:30:47 +0200

    starting product tags

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,16 @@
 class Product < ApplicationRecord
   belongs_to :seller
+
+  after_save :save_tags
   
   has_one_attached :logo
   has_many_attached :images
 
   extend FriendlyId
   friendly_id :name, use: :scoped, scope: :seller
+
+  def save_tags
+    p "called now"
+    raise "error"
+  end
 end
