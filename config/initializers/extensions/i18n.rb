3bddf19fb44c02e5781a0ebcfb6c6dd750fe8131
ingeniousgeorge <ingeniousgeorge@gmail.com>
2019-12-09 12:50:39 +0100

    production bugs

--- config/initializers/extensions/i18n.rb
+++ config/initializers/extensions/i18n.rb
@@ -1,10 +1,12 @@
 module I18n
   
   def self.default_language_hash
-    eval(ENV['DEFAULT_LANGUAGE']) #"{en: 'english'}"
+    # eval(ENV['DEFAULT_LANGUAGE']) #"{en: 'english'}"
+    {en: 'english'}
   end
 
   def self.non_default_locales_hash 
-    eval(ENV['OTHER_LANGUAGES']) #"{fr: 'french'}"
+    # eval(ENV['OTHER_LANGUAGES']) #"{fr: 'french'}"
+    {fr: 'french'}
   end
 end
