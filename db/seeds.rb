3e842d0f073fe6088e3a746f03b0e64a7c00f02b
ingeniousgeorge <ingeniousgeorge@gmail.com>
2019-12-09 02:24:36 +0100

    unlocked seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-if Rails.env.development?
+if Rails.env.development? || Rails.env.production?
 
   lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
   
